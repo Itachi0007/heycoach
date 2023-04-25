090b966ad8c7ddd90ab1d793a47194c3f8fbca4a
akshay_gaikwad <akshay240695@gmail.com>
2023-04-25 19:38:48 +0530

    JS DOM

--- jj.js
+++ jj.js
@@ -17,8 +17,39 @@ var alto = {
 	number: 9912134,
 };
 
-var name = document.getElementById();
+let dom = document.getElementById("heading");
+// console.log(dom);
 
-for (var i = 0; i < 2; i++) {
-	watches[i];
+
+
+
+// Create a new <li> element:
+const element = document.createElement("li");
+
+// Create a new text node:
+const textNode = document.createTextNode("Water");
+
+// Append the text node to the <li> element:
+element.appendChild(textNode);
+
+// Get the <ul> element with id="myList":
+
+// Replace the first child node with the new <li> element:
+
+// function change() {
+// 	// dom.replaceChild(h4, h1)
+// 	// const heading = document.getElementById("heading");
+// 	element.style.color = "red";
+// 	dom.replaceChild(element, dom.childNodes[0]);
+// }
+const x = document.querySelectorAll("head1");
+console.log(x);
+
+document.getElementById("btn").onclick = function(){
+	element.style.color = "red";
+	dom.replaceChild(element, dom.childNodes[0]);
+	
+	let ele = document.getElementsByTagName("h1");
+	console.log(ele);
+	ele[0].textContent = "ABC";
 }
